1af1bf88dc3b5c569369a2995735975cb161c0d4
Axel Lindeberg <alindeb@kth.se>
2017-10-21 14:05:05 +0200

    Update readme, small code changes

--- GameOfLife/GameOfLife/main.cpp
+++ GameOfLife/GameOfLife/main.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Graphics.hpp>
 
-#define WIDTH  200
-#define HEIGHT 200
+#define WINDOW_WIDTH  800
+#define WINDOW_HEIGHT 800
+
+#define CELL_SIZE 4
+#define WIDTH  WINDOW_WIDTH / CELL_SIZE
+#define HEIGHT WINDOW_WIDTH / CELL_SIZE
+
 
 #define DEAD   75
 #define ALIVE  0
-#define CELL_SIZE 4
 
 sf::Uint8 *cells      = new sf::Uint8[WIDTH * HEIGHT];
 sf::Uint8 *cells_prev = new sf::Uint8[WIDTH * HEIGHT];
@@ -17,95 +21,79 @@ int get_index(int x, int y) {
 
 int adjacent(int x, int y) {
     int num_adjacent = 0;
-    
     if ( cells_prev[ get_index(x-1, y-1) ] == ALIVE ) ++num_adjacent;
-    if ( cells_prev[ get_index(x  , y-1) ] == ALIVE ) ++num_adjacent; // row above
+    if ( cells_prev[ get_index(x  , y-1) ] == ALIVE ) ++num_adjacent;
     if ( cells_prev[ get_index(x+1, y-1) ] == ALIVE ) ++num_adjacent;
-    
-    if ( cells_prev[ get_index(x-1, y)   ] == ALIVE ) ++num_adjacent; // our row
+    if ( cells_prev[ get_index(x-1, y)   ] == ALIVE ) ++num_adjacent;
     if ( cells_prev[ get_index(x+1, y)   ] == ALIVE ) ++num_adjacent;
-    
     if ( cells_prev[ get_index(x-1, y+1) ] == ALIVE ) ++num_adjacent;
-    if ( cells_prev[ get_index(x  , y+1) ] == ALIVE ) ++num_adjacent; // row below
+    if ( cells_prev[ get_index(x  , y+1) ] == ALIVE ) ++num_adjacent;
     if ( cells_prev[ get_index(x+1, y+1) ] == ALIVE ) ++num_adjacent;
     return num_adjacent;
 }
 
 void update_cell(int x, int y) {
     int num_adjacent = adjacent(x,y);
+    int i = get_index(x,y);
     
-    if (cells[get_index(x,y)] == DEAD) {
+    if (cells[i] == DEAD) {
         if (num_adjacent == 3) {
-            cells[ get_index(x,y) ] = ALIVE;
+            cells[i] = ALIVE;
         }
     } else if (num_adjacent < 2 || num_adjacent > 3) {
-        cells[ get_index(x,y) ] = DEAD;
+        cells[i] = DEAD;
     }
     
 }
 
-void update_cells() {
-    for (int x = 1; x < WIDTH-1; ++x) {
-        for (int y = 1; y < HEIGHT-1; ++y) {
-            update_cell(x,y);
-        }
-    }
-}
-
-void draw_pixels(int x, int y) {
-    sf::Uint8 dead_alive = cells[get_index(x, y)];
-    
+void draw_cell(int x, int y, sf::Uint8 color) {
     for (int i = 0; i < CELL_SIZE; ++i) {
         for (int j = 0; j < CELL_SIZE; ++j) {
-            int index = CELL_SIZE * (WIDTH * (CELL_SIZE * y + j) + x) + i;
-            pixels[4*index]   = dead_alive;
-            pixels[4*index+1] = dead_alive;
-            pixels[4*index+2] = dead_alive;
-            pixels[4*index+3] = 255;
-        }
-    }
-}
-
-void update_pixels() {
-    for (int x = 0; x < WIDTH; ++x) {
-        for (int y = 0; y < HEIGHT; y++) {
-            draw_pixels(x,y);
+            int index = 4 * (CELL_SIZE * (WIDTH * (CELL_SIZE * y + j) + x) + i);
+            pixels[index++] = color;
+            pixels[index++] = color;
+            pixels[index++] = color;
+            pixels[index++] = 255;
         }
     }
 }
 
 void update() {
     std::copy(cells, cells + WIDTH * HEIGHT, cells_prev);
-    update_cells();
-    update_pixels();
+    
+    for (int x = 1; x < WIDTH-1; ++x) {
+        for (int y = 1; y < HEIGHT-1; ++y) {
+            update_cell(x,y);
+        }
+    }
+    for (int x = 0; x < WIDTH; ++x) {
+        for (int y = 0; y < HEIGHT; y++) {
+            draw_cell(x,y, cells[get_index(x, y)]);
+        }
+    }
 }
 
 int main() {
-    srand(time(NULL));
-    for (int y = 0; y < HEIGHT; ++y) {
-        for (int x = 0; x < WIDTH; ++x) {
-            cells[ get_index(x,y) ] = rand() % 2 == 0 ? ALIVE : DEAD;
-        }
+    if (WINDOW_WIDTH % CELL_SIZE != 0 || WINDOW_HEIGHT % CELL_SIZE != 0) return 1;
+    
+    srand((unsigned int) time(NULL));
+    for (int i = 0; i < WIDTH * HEIGHT; ++i) {
+        cells[i] = rand() % 2 == 0 ? ALIVE : DEAD;
     }
-    sf::Texture txt;
-    txt.create(CELL_SIZE * WIDTH, CELL_SIZE * HEIGHT);
     
     sf::Sprite sprite;
+    sf::Texture txt;
+    txt.create(WINDOW_WIDTH, WINDOW_HEIGHT);
     
-    sf::RenderWindow window(sf::VideoMode(CELL_SIZE * WIDTH, CELL_SIZE * HEIGHT), "Game of Life");
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Game of Life");
     bool paused = false;
+    
     while(window.isOpen()) {
         sf::Event e;
         while(window.pollEvent(e)) {
             switch (e.type) {
                 case sf::Event::Closed:
                     window.close();
-                    break;
-                case sf::Event::LostFocus:
-                    paused = true;
-                    break;
-                case sf::Event::GainedFocus:
-                    paused = false;
                     break;
                 case sf::Event::MouseButtonPressed:
                     paused = !paused;
@@ -114,36 +102,17 @@ int main() {
                     break;
             }
         }
-        window.clear();
+        sf::sleep(sf::milliseconds(20));
         
-        if (!paused) update();
+        if (paused) continue;
         
+        update();
         txt.update(pixels);
         sprite.setTexture(txt);
         window.draw(sprite);
-        window.display();
         
-        //sf::sleep(sf::milliseconds(30));
+        window.display();
     }
+    
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Game-of-Life
-Game of life in c++ with SFML
+This is a simple clone of Conways famous Game of Life.
+Created using [SFML](https://www.sfml-dev.org/) and written in C++.
+
+![](./screenshot.png)
