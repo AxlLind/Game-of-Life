e02d73455f3134fe46ccf1d70c9b90bf534c4a12
Axel Lindeberg <alindeb@kth.se>
2017-10-29 11:26:38 +0100

    Add color-mode

--- Julia-Set/Julia-Set/main.cpp
+++ Julia-Set/Julia-Set/main.cpp
@@ -1,18 +1,18 @@
 #include <SFML/Graphics.hpp>
 
 #define WINDOW_SIZE 800
-#define ITERATIONS 100
-#define MANDELBROTH 3
+#define ITERATIONS 255
+#define MANDELBROTH 5
 
 sf::Uint8 *cells =  new sf::Uint8[WINDOW_SIZE * WINDOW_SIZE];
 sf::Uint8 *pixels = new sf::Uint8[WINDOW_SIZE * WINDOW_SIZE * 4];
 
 
-bool draw = true, follow_mouse = false;
-double min = -2, max = 2;
+bool draw = true, follow_mouse = false, color_mode = false;
+double max = 2.0, min = -max;
 double set_a = MANDELBROTH, set_b = MANDELBROTH;
 
-double to_complex(int x) {
+double pixel_to_coord(int x) {
     return (max - min) * double(x) / WINDOW_SIZE + min;
 }
 
@@ -35,22 +35,48 @@ int iterations(double x, double y) {
 void calculate_set() {
     for (int y = 0; y < WINDOW_SIZE; ++y) {
         for (int x = 0; x < WINDOW_SIZE; ++x) {
-            double a = to_complex(x);
-            double b = to_complex(y);
+            double a = pixel_to_coord(x);
+            double b = pixel_to_coord(y);
             cells[y*WINDOW_SIZE + x] = iterations(a, b);
         }
     }
 }
 
+void calc_rgb(int *color, int val) {
+    if (!color_mode) {
+        color[0] = val * 255 / ITERATIONS;
+        color[1] = color[0]; color[2] = color[0];
+        return;
+    }
+    
+    double r = 1.0, g = 1.0, b = 1.0, iter = double(val);
+    if (iter < (0.2 * ITERATIONS)) {
+        r = 0;
+        g = 4.0 * iter / ITERATIONS;
+    } else if (iter < (0.4 * ITERATIONS)) {
+        r = 0;
+        b = 1.0 + 4 * (0.25 * ITERATIONS - iter) / ITERATIONS;
+    } else if (iter < (0.5 * ITERATIONS)) {
+        r = 4 * (iter - 0.5 * ITERATIONS) / ITERATIONS;
+        b = 0;
+    } else {
+        g = 1.0 + 4.0 * (0.6 * ITERATIONS - iter) / ITERATIONS;
+        b = 0;
+    }
+    
+    color[0] = int(255*r); color[1] = int(255*g); color[2] = int(255*b);
+}
+
 void calculate_pixels() {
-    int step = 255 / ITERATIONS;
     for (int i = 0; i < WINDOW_SIZE * WINDOW_SIZE; ++i) {
+        int color[] = {0,0,0};
+        if (cells[i] != ITERATIONS && cells[i] != 0) calc_rgb(color, cells[i]);
+        
         int index = 4 * i;
-        int c = cells[i] == ITERATIONS ? 0 : cells[i];
-        pixels[index++] = step * c;
-        pixels[index++] = step * c;
-        pixels[index++] = step * c;
-        pixels[index++] = 255;
+        pixels[index++] = color[0];
+        pixels[index++] = color[1];
+        pixels[index++] = color[2];
+        pixels[index] = 255;
     }
 }
 
@@ -82,14 +108,18 @@ void handle_key_press(sf::Keyboard::Key code) {
         case sf::Keyboard::M:
             follow_mouse = !follow_mouse;
             break;
+        case sf::Keyboard::C:
+            color_mode = !color_mode;
+            break;
         case sf::Keyboard::Escape:
             set_a = MANDELBROTH;
             set_b = MANDELBROTH;
-            min = -2;
-            max =  2;
+            max =  2.0;
+            min = -max;
+            color_mode = false;
+            follow_mouse = false;
             break;
-        default:
-            break;
+        default: break;
     }
 }
 
@@ -112,15 +142,13 @@ int main() {
                     handle_key_press(e.key.code);
                     redraw();
                     break;
-                default:
-                    break;
+                default: break;
             }
         }
-        
         if (follow_mouse) {
             mouse_pos = sf::Mouse::getPosition(window);
-            set_a = to_complex(mouse_pos.x);
-            set_b = to_complex(mouse_pos.y);
+            set_a = pixel_to_coord(mouse_pos.x);
+            set_b = pixel_to_coord(mouse_pos.y);
             redraw();
         }
         
